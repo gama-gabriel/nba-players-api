65361eaa63fdb1e95fd46f5b6276acec513fd4d6
gama-gabriel <bielmgama@gmail.com>
2023-09-03 00:01:27 -0300

    changed the structure to allow deployment

--- api/players_api.py
+++ api/players_api.py
@@ -1,20 +1,24 @@
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS, cross_origin
-from db import Players, Player_images
+from flask import Flask, json, jsonify
+from os.path import join
 
 app = Flask(__name__)
-cors = CORS(app)
 app.json.sort_keys = False 
 
-@app.route('/players', methods = ['GET'])
-@cross_origin()
-def get_players():
-    return make_response(jsonify(Players))
+@app.route('/')
+def guia():
+    return 'Set the url to /players or /player_images to get the results'
 
-@app.route('/player_images', methods = ['GET'])
-@cross_origin()
+@app.route('/players/', methods = ['GET'])
+def get_players():
+    with open ('dados/players.json', 'r') as file:
+        players = json.load(file)
+        return jsonify(players)
+
+@app.route('/player_images/', methods = ['GET'])
 def get_player_images():
-    return make_response(jsonify(Player_images))
+    with open ('dados/player_images.json', 'r') as file:
+        player_images = json.load(file)
+        return jsonify(player_images)
 
 if __name__ == '__main__':
     app.run()
