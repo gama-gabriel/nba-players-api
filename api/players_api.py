5b44355bec5e909bceaf8fcbdbfc5cd23c2339df
gama-gabriel <bielmgama@gmail.com>
2023-09-04 19:50:59 -0300

    added teams images to api

--- api/players_api.py
+++ api/players_api.py
@@ -1,7 +1,9 @@
 from flask import Flask, json, jsonify
 from os.path import join
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 app.json.sort_keys = False 
 
 @app.route('/')
@@ -9,14 +11,23 @@ def guia():
     return 'Set the url to /players or /player_images to get the results'
 
 @app.route('/players/', methods = ['GET'])
+@cross_origin()
 def get_players():
     with open ('dados/players.json', 'r') as file:
         players = json.load(file)
         return jsonify(players)
 
 @app.route('/player_images/', methods = ['GET'])
+@cross_origin()
 def get_player_images():
     with open ('dados/player_images.json', 'r') as file:
+        player_images = json.load(file)
+        return jsonify(player_images)
+    
+@app.route('/teams/', methods = ['GET'])
+@cross_origin()
+def get_teams():
+    with open ('dados/teams.json', 'r') as file:
         player_images = json.load(file)
         return jsonify(player_images)
 
